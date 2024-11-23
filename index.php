cb5efae13cd653359aa8fed678873d1db3dfdc0b
Mohammed <69204096+Mohammed27800@users.noreply.github.com>
2024-11-23 23:26:52 +0300

    functions and filters

--- index.php
+++ index.php
@@ -19,12 +19,27 @@
             "name"=>"Atomic Habits",
             "purchaseURL"=>"https://a.co/d/arWBEZt"
          ],
-    ]
+        ];
+
+    function Filter($Items,$fn){
+        $FilteredItems = [];
+        foreach($Items as $Item){
+            if($fn($Item)){
+                $FilteredItems[] = $Item;
+            }
+        }
+        return $FilteredItems;
+    }
+
+    $FilteredBooks = array_filter($Books, function($Book) {
+        return $Book['author'] === "James Clear" || $Book['name'] === "Rich dad, Poor dad";
+    });
+
     ?>
     <ul>
-         <?php foreach($Books as $Book) : ?>
+         <?php foreach($FilteredBooks as $Book) : ?>
             <a href="<?= $Book['purchaseURL'] ?>">
-            <li><?= $Book['name'] ?></li>
+            <li><?= $Book['name'] ?> <sub>by <?= $Book['author'] ?></sub></li>
             </a>
          <?php endforeach ?>
     </ul>
