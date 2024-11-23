3b1aaad901aef3af928c6d4317beec5ff2dd3e7d
Mohammed <69204096+Mohammed27800@users.noreply.github.com>
2024-11-23 21:57:08 +0300

    7 Arrays

--- index.php
+++ index.php
@@ -6,8 +6,18 @@
     <title>Document</title>
 </head>
 <body>
+    <h1>Welcome to this recommendation System</h1>
     <?php 
-    echo "Hello World";
+    $Books = [
+        "Atomic Habits",
+        "Rich dad, Poor dad",
+        "GitPro"
+    ]
     ?>
+    <ul>
+         <?php foreach($Books as $Book) : ?>
+         <li><?= $Book ?></li>
+         <?php endforeach ?>
+    </ul>
 </body>
 </html>
