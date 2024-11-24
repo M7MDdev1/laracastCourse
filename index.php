30f27acf329ff849387f7e827f0970b884c6eac8
Mohammed <69204096+Mohammed27800@users.noreply.github.com>
2024-11-24 12:52:26 +0300

    Separate Logic From the Template

--- index.php
+++ index.php
@@ -1,13 +1,5 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <h1>Welcome to this recommendation System</h1>
-    <?php 
+<?php 
+
     $Books = [
         [
             "author"=>"Robert Kiosaki",
@@ -31,17 +23,8 @@
         return $FilteredItems;
     }
 
-    $FilteredBooks = array_filter($Books, function($Book) {
+    $FilteredBooks = Filter($Books, function($Book) {
         return $Book['author'] === "James Clear" || $Book['name'] === "Rich dad, Poor dad";
     });
 
-    ?>
-    <ul>
-         <?php foreach($FilteredBooks as $Book) : ?>
-            <a href="<?= $Book['purchaseURL'] ?>">
-            <li><?= $Book['name'] ?> <sub>by <?= $Book['author'] ?></sub></li>
-            </a>
-         <?php endforeach ?>
-    </ul>
-</body>
-</html>
+    require "index.view.php";
