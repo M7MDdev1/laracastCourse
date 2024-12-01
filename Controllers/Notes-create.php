4b352b9afeb3282d5b434513b6b85d6d9477c919
Mohammed <69204096+Mohammed27800@users.noreply.github.com>
2024-12-01 14:46:39 +0300

    Validator

--- Controllers/Notes-create.php
+++ Controllers/Notes-create.php
@@ -1,19 +1,33 @@
 <?php
 
 
-// dd($_SERVER['REQUEST_METHOD']);
-
 $Name = "Create Note";
+
+require "Validator.php";
 
 $CurrentUser = 1;
 
 if($_SERVER['REQUEST_METHOD'] === "POST")
 {
-    $posts = $db->Query("INSERT INTO Posts(title,UserID) values (:title, :UserID)",[
-    "title"=> $_POST['title'],
-    "UserID"=>$CurrentUser
-    ])->get();
 
+    $error = null;
+
+
+    if(!Validator::string($_POST['title'],7,1000)){
+        $error = "A body of no more than 1000 and not less than 7 is required";
+    }
+
+    
+    
+
+   if(!isset($error)){
+    $posts = $db->Query("INSERT INTO Posts(title,UserID) values (:title, :UserID)",[
+        "title"=> $_POST['title'],
+        "UserID"=>$CurrentUser
+        ])->get();
+   }
+
+    
 }
 
 
--- views/Notes-create.view.php
+++ views/Notes-create.view.php
@@ -8,8 +8,10 @@
   <h1 class="text-3xl">Create Note !</h1>
 
   <form  method="post">
-    <textarea id="title" name="title"></textarea>
-
+    <textarea id="title" name="title"><?= $_POST['title'] ?? "" ?></textarea>
+    <?php if(isset($error)) :?>
+      <p class="mt-5 text-red-500"><?= $error ?></p>
+      <?php endif ?>
     <button class=" p-3 text-white font-bold block bg-blue-600 rounded-full ">Save</button>
   </form>
 
