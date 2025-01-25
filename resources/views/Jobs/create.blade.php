5befcd008e2d2211d6f83b86febb6f2187d82c28
Mohammed27800 <asmte2780@gmail.com>
2025-01-25 17:12:06 +0300

    Make registeration Form

--- resources/views/Jobs/create.blade.php
+++ resources/views/Jobs/create.blade.php
@@ -5,38 +5,39 @@
 
     <form method="POST" action="/jobs">
         @csrf
+
         <div class="space-y-12">
-          <div class="border-b border-gray-900/10 pb-12">
-            <h2 class="text-base/7 font-semibold text-gray-900">Create a new Job post</h2>
+            <div class="border-b border-gray-900/10 pb-12">
+                <h2 class="text-base font-semibold leading-7 text-gray-900">Create a New Job</h2>
+                <p class="mt-1 text-sm leading-6 text-gray-600">We just need a handful of details from you.</p>
 
-            <div class="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
-              <div class="sm:col-span-3">
-                <label for="title" class="block text-sm/6 font-medium text-gray-900">Title</label>
-                <div class="mt-2">
-                  <input type="text" name="title" id="title" class="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6">
-                @error('title')
-                <p class="text-xs text-red-500 font-semibold">{{$message}}</p>
-                @enderror
-                </div>
-              </div>
+                <div class="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
+                    <x-form-field>
+                        <x-form-label for="title">Title</x-form-label>
 
-              <div class="sm:col-span-3">
-                <label for="salary" class="block text-sm/6 font-medium text-gray-900">Salary</label>
-                <div class="mt-2">
-                  <input type="text" name="salary" id="salary" autocomplete="family-name" class="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6">
-                  @error('salary')
-                  <p class="text-xs text-red-500 font-semibold">{{$message}}</p>
-                  @enderror
+                        <div class="mt-2">
+                            <x-form-input name="title" id="title" placeholder="CEO" />
+
+                            <x-form-error name="title" />
+                        </div>
+                    </x-form-field>
+
+                    <x-form-field>
+                        <x-form-label for="salary">Salary</x-form-label>
+
+                        <div class="mt-2">
+                            <x-form-input name="salary" id="salary" placeholder="$50,000 USD" />
+
+                            <x-form-error name="salary" />
+                        </div>
+                    </x-form-field>
                 </div>
-              </div>
             </div>
-          </div>
         </div>
 
         <div class="mt-6 flex items-center justify-end gap-x-6">
-          <button type="button" class="text-sm/6 font-semibold text-gray-900">Cancel</button>
-          <button type="submit" class="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Save</button>
+            <button type="button" class="text-sm font-semibold leading-6 text-gray-900">Cancel</button>
+            <x-form-button>Save</x-form-button>
         </div>
-      </form>
-
+    </form>
 </x-layout>
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use App\Http\Controllers\JobController;
+use App\Http\Controllers\sessionController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 Route::view('/','Home');
@@ -9,3 +11,8 @@ Route::resource('jobs', JobController::class);
 
 Route::view('/Contact','Contact');
 
+Route::get('/register', [UserController::class, 'create']);
+
+Route::post('/register',[UserController::class,'store']);
+Route::get('/login', [sessionController::class, 'index']);
+Route::post('/login',[sessionController::class,'store']);
